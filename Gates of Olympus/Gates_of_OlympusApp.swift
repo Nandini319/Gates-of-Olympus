4477fe3a844cb51fd1a0854db8376f67ee98d581
Nandini Vithlani <nandinivithlani99@gmail.com>
2023-10-06 16:03:45 +0530

    Initial Commit

---
+++ Gates of Olympus/Gates_of_OlympusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gates_of_OlympusApp.swift
+//  Gates of Olympus
+//
+//  Created by Nandini Vithlani on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Gates_of_OlympusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
